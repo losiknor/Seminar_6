c9282f689ae1542cfb60e9a7d60aa8b8784028cc
Larina <Losiknor@gmail.com>
2022-03-02 14:51:51 +0700

    Решение задачи 41

---
+++ zadacha_41/Program.cs
@@ -0,0 +1,8 @@
+﻿// 41. Выяснить являются ли три числа сторонами треугольника
+int a = new Random().Next(1, 100);
+int b = new Random().Next(1, 100);
+int c = new Random().Next(1, 100);
+Console.Write($"Три числа: {a}  {b}  {c}");
+if ((a + b > c) && (b + c > a) && (a + c > b))
+Console.WriteLine("  являются сторонами треугольника.");
+else Console.WriteLine("  не являются сторонами треугольника.");
