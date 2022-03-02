d7e4bd0855828be54e9208c7781d7308a3acbd91
Larina <Losiknor@gmail.com>
2022-03-02 15:16:25 +0700

    Решение задачи 45

---
+++ zadacha_45/Program.cs
@@ -0,0 +1,16 @@
+﻿// 45. Показать числа Фибоначчи
+int n = new Random().Next(3, 10);
+int x = 1;
+int y = 1;
+Console.Write($"Числа Фибоначчи:  {x}  {y}  ");
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(x + y);
+    Console.Write("  ");
+    int xtmp = x;
+    x = y;
+    y = xtmp + y;
+}
+
+
+
