207f0cc97fac8e949232c2b9d507f4b1d4f9da2b
Larina <Losiknor@gmail.com>
2022-03-02 17:16:32 +0700

    Решение задачи 47

--- zadacha_47/Program.cs
+++ zadacha_47/Program.cs
@@ -1,2 +1,17 @@
 ﻿// 47. Написать программу копирования массива
-Console.WriteLine("Hello, World!");
+int n = new Random().Next(3, 15);
+int[] array = new int[n];
+Console.WriteLine("Дан массив array:");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(30, 150);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+int[] arrayCopy = new int[array.Length];
+Console.WriteLine("Его копия arrayCopy:");
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy[i] = array[i];
+    Console.Write(arrayCopy[i] + " ");
+}
